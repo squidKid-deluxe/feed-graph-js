0d257b7ec94f14e03b127908178034578627fd36
squiddible <63127872+squidKid-deluxe@users.noreply.github.com>
2026-02-15 17:02:51 -0500

    Update endpoints; migrate to es.bitshares.dev

--- feed_graph.js
+++ feed_graph.js
@@ -63,22 +63,15 @@ function publicNodes() {
     use these nodes for all operations
     */
     return [
-        "wss://dex.iobanker.com/ws",
-        "wss://api.bts.mobi/wss",
-        "wss://bitsharesapi.loclx.io/ws",
-        "wss://api-us.61bts.com/ws",
-        "wss://btsapi.magicw.net/wss",
-        "wss://api.dex.trading/ws",
+        "wss://api.bitshares.dev/wss",
+        "wss://api.btslebin.com/ws",
+        "wss://api.dex.trading",
+        "wss://public.xbts.io/wss",
         "wss://btsws.roelandp.nl/ws",
-        "wss://eu.nodes.bitshares.ws/ws",
-        "wss://public.xbts.io/ws",
-        "wss://node.xbts.io/ws",
-        "wss://bts.mypi.win/ws",
-        "wss://cloud.xbts.io/wss",
-        "wss://api.61bts.com/wss",
-        "wss://api.btslebin.com/wss",
-        "wss://api.bts.btspp.io:10100/ws",
-    ]
+        "wss://cloud.xbts.io/ws",
+        "wss://dex.iobanker.com/ws",
+        "wss://node.xbts.io/wss"
+   ]
 }
 
 async function wssHandshake() {
@@ -244,7 +237,7 @@ async function settlement_price(rpc, data) {
 }
 
 async function kibana(start, stop, tokens) {
-    const url = `https://es.bts.mobi/bitshares-*/_async_search`;
+    const url = `https://es.bitshares.dev/bitshares-*/_async_search`;
 
     const headers = {
         'Content-Type': 'application/json'
